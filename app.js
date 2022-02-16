3528b3e05f05d918af5cc3874d794ef585f5a9d0
Carlos Martinez <Carlos.martdelgado@gmail.com>
2022-02-16 20:33:16 +0100

    Lab Dani-Carlos1

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+const randomBeer = punkAPI.getRandom()
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -21,5 +23,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+//BEERS RUTA
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beersFromApi => {
+    //console.log('Beers from the database: ', beersFromApi)
+    res.render("beers.hbs", {
+      beersFromApi: beersFromApi
+    })
+ 
+  })
+  .catch(error => console.log(error));
+});
+
+//RANDOM RUTA
+   app.get('/random-beer', (req, res) => {
+    punkAPI.getRandom()    
+    .then(responseFromApi => {
+      console.log(responseFromApi)
+      res.render("random-beer.hbs", {
+        responseFromApi: responseFromApi
+      })
+  
+    })
+    .catch(error => console.log(error));
+  });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
